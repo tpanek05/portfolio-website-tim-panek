28b69c3a371f65c2018378ff01e8b71a55324691
tpanek05 <74510264+tpanek05@users.noreply.github.com>
2020-12-20 19:08:57 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
